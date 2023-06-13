b2a08be3f0281aec14faf8fedac70478e9aff1dd
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-13 22:55:06 +0000

    Add name field to devise registration. Update the config in Application Controllers to permit names on sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |user| user.permit(:name, :email, :password, :current_password) }
+  end
 end
+  
