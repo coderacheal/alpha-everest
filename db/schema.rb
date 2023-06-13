16ee636a8d0eaa85da853fc9291501150471c1bd
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-13 22:25:02 +0000

    Set up associations and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_175418) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_215531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_expenses", force: :cascade do |t|
+    t.bigint "categories_id", null: false
+    t.bigint "expenses_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["categories_id"], name: "index_categories_expenses_on_categories_id"
+    t.index ["expenses_id"], name: "index_categories_expenses_on_expenses_id"
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "name"
@@ -31,4 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_175418) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories_expenses", "categories", column: "categories_id"
+  add_foreign_key "categories_expenses", "expenses", column: "expenses_id"
 end
