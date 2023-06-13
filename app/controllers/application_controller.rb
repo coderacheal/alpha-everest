6c7025a6aa4699534e3f603bd873e2fc486cc524
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-13 23:15:35 +0000

    Add model and authenticating user before accessing certain views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:name, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) { |user| user.permit(:name, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+      user.permit(:name, :email, :password, :current_password)
+    end
   end
 end
-  
