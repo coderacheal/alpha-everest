8ca5e292e7ff3cc36a96d0ab231e2cc38b31954c
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-15 18:21:52 +0000

    Add create action to add new expense to a particular category

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,6 +5,24 @@ class ExpensesController < ApplicationController
     end
 
     def new
+        @category = Category.find(params[:category_id])
+        @expense = Expense.new
+    end
+
+    def create
+        @category = Category.find(params[:category_id])
+        @expense = @category.expenses.build(expense_params)
+        if @expense.save
+            redirect_to user_category_expenses_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def expense_params
+        params.require(:expense).permit(:name, :amount).merge(author_id: current_user.id)
     end
   end
   
