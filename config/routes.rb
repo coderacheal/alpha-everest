d34a3afd43f677337bdb96757be6076285b7dfc0
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-16 19:13:50 +0000

    Fix route on dev

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> dev
   get '/home', to: 'home#index'
   
   resources :categories do
@@ -13,11 +9,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'home#index'
-<<<<<<< HEAD
-=======
   # Defines the root path route ("/")
   # root "articles#index"
->>>>>>> 34decbd856036949981fb5a66e9e33c6050db301
-=======
->>>>>>> dev
 end
